f98fe08575363bf97c52500d5eca3cd8595c5c45
Denver <131719993+modzbosss02@users.noreply.github.com>
2025-08-13 23:17:27 -0500

    Update sw.js
    updated bug free (white screen ts)

--- sw.js
+++ sw.js
@@ -7,13 +7,16 @@ const urlsToCache = [
   "/icon-512.png"
 ];
 
+// Cache essential files on install
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting(); // Activate worker immediately
 });
 
+// Serve files from cache first, then network
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
@@ -21,6 +24,7 @@ self.addEventListener("fetch", event => {
   );
 });
 
+// Remove old caches on activate
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys => 
@@ -30,4 +34,5 @@ self.addEventListener("activate", event => {
       )
     )
   );
+  self.clients.claim(); // Take control of all pages
 });
